1cdfeb24016b0843c80e986eb5b3b93dd920a88e
Daniil Fedotov <fedotov.danil@gmail.com>
2017-03-15 16:42:34 +0000

    Correct types when using msg store clients with module tag
    variable queue state stores msg store clients with message store module tags

--- test/backing_queue_SUITE.erl
+++ test/backing_queue_SUITE.erl
@@ -376,10 +376,11 @@ on_disk_stop(Pid) ->
 
 msg_store_client_init_capture(MsgStore, Ref) ->
     Pid = spawn(fun on_disk_capture/0),
-    {Pid, rabbit_msg_store_vhost_sup:client_init(
+    {rabbit_msg_store, MSCState} = rabbit_msg_store_vhost_sup:client_init(
             MsgStore, Ref, fun (MsgIds, _ActionTaken) ->
                                    Pid ! {on_disk, MsgIds}
-                           end, undefined, <<"/">>)}.
+                           end, undefined, <<"/">>),
+    {Pid, MSCState}.
 
 msg_store_contains(Atom, MsgIds, MSCState) ->
     Atom = lists:foldl(
@@ -456,7 +457,7 @@ test_msg_store_confirm_timer() ->
     Ref = rabbit_guid:gen(),
     MsgId  = msg_id_bin(1),
     Self = self(),
-    MSCState = rabbit_msg_store_vhost_sup:client_init(
+    {rabbit_msg_store, MSCState} = rabbit_msg_store_vhost_sup:client_init(
                  ?PERSISTENT_MSG_STORE, Ref,
                  fun (MsgIds, _ActionTaken) ->
                          case gb_sets:is_member(MsgId, MsgIds) of
@@ -1337,7 +1338,8 @@ nop(_) -> ok.
 nop(_, _) -> ok.
 
 msg_store_client_init(MsgStore, Ref) ->
-    rabbit_msg_store_vhost_sup:client_init(MsgStore, Ref, undefined, undefined, <<"/">>).
+    {rabbit_msg_store, MSCState} = rabbit_msg_store_vhost_sup:client_init(MsgStore, Ref, undefined, undefined, <<"/">>),
+    MSCState.
 
 variable_queue_init(Q, Recover) ->
     rabbit_variable_queue:init(
